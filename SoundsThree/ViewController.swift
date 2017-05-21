323c38594ef78d474a2133b0e1d0d3837b98ec3c
Jennifer Gaspar-Santos <jsantos@siprep.org>
2017-05-21 09:14:49 -0700

    Initial Commit

---
+++ SoundsThree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundsThree
+//
+//  Created by jsantos on 5/21/17.
+//  Copyright © 2017 jsantos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
